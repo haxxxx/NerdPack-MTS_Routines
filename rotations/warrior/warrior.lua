1aa14323dbe06c0100d4372e4f722fe7e21af69c
Jaime Marques <mrthesoulz@gmail.com>
2016-07-23 13:39:41 +0100

    Update Warrior Basic CR

--- rotations/warrior/warrior.lua
+++ rotations/warrior/warrior.lua
@@ -1,4 +1,17 @@
+local Keybinds = {
+	{'Charge', {
+		'modifier.shift',
+		'target.range >= 8',
+		'target.range <= 25'
+	}, 'target'}
+}
+
 NeP.Engine.registerRotation(1, '[|cff'..NeP.Interface.addonColor..'NeP|r] Warrior - Basic', 
 {-- In-Combat CR
-	{'Heroic Strike'}
-}, outCombat, exeOnLoad)
+	{Keybinds},
+	{'Victory Rush'},
+	{'Execute', 'target.health <= 20'},
+	{'Slam'}
+},{-- Ouside of combat
+	{Keybinds}
+}, exeOnLoad)
