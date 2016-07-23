a919806cf4cb7214528246340ad13ee071ba50bc
Jaime Marques <mrthesoulz@gmail.com>
2016-07-23 16:26:48 +0100

    oops

--- rotations/hunter/hunter.lua
+++ rotations/hunter/hunter.lua
@@ -1,5 +1,6 @@
 NeP.Engine.registerRotation(3, '[|cff'..NeP.Interface.addonColor..'NeP|r] Hunter - Basic', 
 {-- In-Combat CR
+	{'Kill Command', 'target.petinmelee'},
 	{'Cobra Shot'}
 },{-- Ouside of combat
 	
