3928faf1fcced96f9f20676c5cef310d48b4f17e
Jaime Marques <mrthesoulz@gmail.com>
2016-07-24 15:34:39 +0100

    init monk-brew

--- rotations/monk/brewmaster.lua
+++ rotations/monk/brewmaster.lua
@@ -34,7 +34,16 @@ local AoE = {
 }
 
 local ST = {
-
+	--Keg Smash on cooldown.
+	{'Keg Smash'},
+	--Breath of Fire on cooldown.
+	{'Breath of Fire'},
+	--Expel Harm when you can use the heal and have 3 or more spheres.
+	--{'Expel Harm'},
+	--Blackout Strike on cooldown.
+	{'Blackout Strike'},
+	--Tiger Palm to dump Energy.
+	{'Tiger Palm', 'player.energy > 75'}
 }
 
 local Keybinds = {
