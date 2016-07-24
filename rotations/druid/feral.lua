6541382d321bab5de4b4bd039bea287d5f481baf
Jaime Marques <mrthesoulz@gmail.com>
2016-07-24 19:57:04 +0100

    init all druids

---
+++ rotations/druid/feral.lua
@@ -0,0 +1,56 @@
+local mKey = 'MTSDdFrl'
+local config = {
+	key = mKey,
+	profiles = true,
+	title = '|T'..MTS.Interface.Logo..':10:10|t MTS Config',
+	subtitle = 'Druid Feral Settings',
+	color = NeP.Core.classColor('player'),
+	width = 250,
+	height = 500,
+	config = {
+
+	}
+}
+
+NeP.Interface.buildGUI(config)
+local E = MTS.dynEval
+local F = function(key) return NeP.Interface.fetchKey(mKey, key, 100) end
+
+local exeOnLoad = function()
+	MTS.Splash()
+	MTS.ClassSetting(mKey)
+end
+
+local Survival = {
+
+}
+
+local Cooldowns = {
+
+}
+
+local AoE = {
+
+}
+
+local ST = {
+
+}
+
+local Keybinds = {
+	-- Pause
+	{'pause', 'modifier.alt'},
+}
+
+local outCombat = {
+	{Keybinds},
+}
+
+NeP.Engine.registerRotation(103, '[|cff'..MTS.Interface.addonColor..'MTS|r] Druid - Feral', 
+	{-- In-Combat
+		{Keybinds},
+		{Survival, 'player.health < 100'},
+		{Cooldowns, 'modifier.cooldowns'},
+		{AoE, 'player.area(8).enemies >= 3'},
+		{ST}
+	}, outCombat, exeOnLoad)
