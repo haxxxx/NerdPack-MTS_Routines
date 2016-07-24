a1e3c99c68440e3638b20a94ffdedf7ac312ca25
Jaime Marques <mrthesoulz@gmail.com>
2016-07-24 18:37:38 +0100

    Init DK unholy

--- rotations/deathknight/unholy.lua
+++ rotations/deathknight/unholy.lua
@@ -34,7 +34,16 @@ local AoE = {
 }
 
 local ST = {
-
+	--Virulent Plague maintained at all times via Outbreak.
+	{'Outbreak', '!target.debuff(Virulent Plague)'},
+	--Death Coil with Sudden Doom procs.
+	{'Death Coil', 'player.buff(Sudden Doom)'},
+	--Scourge Strike to burst Festering Wound.
+	{'Scourge Strike', 'target.debuff(Festering Wound)'},
+	--Festering Strike to apply Festering Wound.
+	{'Festering Strike', '!target.debuff(Festering Wound)'},
+	--Death Coil to dump Runic Power.
+	{'Death Coil'}
 }
 
 local Keybinds = {
