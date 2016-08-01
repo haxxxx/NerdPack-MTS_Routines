15c190328caccb0684ff9e1b0bfad4de90a6dc44
Jaime Marques <mrthesoulz@gmail.com>
2016-08-01 23:22:07 +0100

    Hunter BM and MM

--- rotations/hunter/bm.lua
+++ rotations/hunter/bm.lua
@@ -26,15 +26,31 @@ local Survival = {
 }
 
 local Cooldowns = {
+	--Bestial Wrath Use on cooldown.
+	{'Bestial Wrath'},
+	--Aspect of the Wild Use on cooldown.
+	{'Aspect of the Wild'}
+}
 
+local Pet = {
+	-- Mend Pet
+	{'Mend Pet', 'pet.health < 100'},
+	--Kill Command on cooldown.
+	{'Kill Command', 'target.petrange < 25'},
 }
 
 local AoE = {
-
+	--Dire Beast on cooldown.
+	{'Dire Beast'},
+	--Multi-Shot for Beast Cleave and to dump Focus.
+	{'Multi-Shot'}
 }
 
 local ST = {
-
+	--Cobra Shot as needed to dump Focus.
+	{'Cobra Shot', 'player.focus > 65'},
+	--Dire Beast on cooldown to generate Focus.
+	{'Dire Beast'}
 }
 
 local Keybinds = {
@@ -51,6 +67,7 @@ NeP.Engine.registerRotation(253, '[|cff'..MTS.Interface.addonColor..'MTS|r] Hunt
 		{Keybinds},
 		{Survival, 'player.health < 100'},
 		{Cooldowns, 'modifier.cooldowns'},
+		{pet, {'pet.exists', 'pet.alive'}},
 		{AoE, 'player.area(8).enemies >= 3'},
 		{ST}
 	}, outCombat, exeOnLoad)
--- rotations/hunter/mm.lua
+++ rotations/hunter/mm.lua
@@ -26,15 +26,37 @@ local Survival = {
 }
 
 local Cooldowns = {
+	-- Trueshot Use on every cooldown.
+	{'Trueshot'}
+}
 
+local Pet = {
+	-- Mend Pet
+	{'Mend Pet', 'pet.health < 100'},
 }
 
 local AoE = {
-
+	--Marked Shot whenever available.
+	{'Marked Shot'},
+	--Multi-Shot to dump Focus. Try to align with Bombardment procs.
+	{'Multi-Shot', {'player.buff(Bombardment)', 'or', 'player.focus > 65'}}
 }
 
 local ST = {
-	
+	--Marked Shot to maintain Vulnerable.
+	{'Marked Shot', {
+		'!target.debuff(Vulnerable).count >= 3', 
+		'or', -- OR
+		'target.debuff(Vulnerable).duration < 8'
+	}, 'target'},
+	--Aimed Shot with Lock and Load or to dump excess Focus.
+	{'Aimed Shot', {
+		'player.buff(Lock and Load)',
+		'or', -- OR
+		'player.focus > 65'
+	}, 'target'},
+	--Arcane Shot to build Focus.
+	{'Arcane Shot'}
 }
 
 local Keybinds = {
@@ -51,6 +73,7 @@ NeP.Engine.registerRotation(254, '[|cff'..MTS.Interface.addonColor..'MTS|r] Hunt
 		{Keybinds},
 		{Survival, 'player.health < 100'},
 		{Cooldowns, 'modifier.cooldowns'},
+		{pet, {'pet.exists', 'pet.alive'}},
 		{AoE, 'player.area(8).enemies >= 3'},
 		{ST}
 	}, outCombat, exeOnLoad)
