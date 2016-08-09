20ada9d3695eb693a7e52040e8d6216059b123d3
Jaime Marques <mrthesoulz@gmail.com>
2016-08-09 14:13:21 +0100

    add init priest shadow

--- rotations/priest/shadow.lua
+++ rotations/priest/shadow.lua
@@ -21,12 +21,38 @@ local lib = function()
 	MTS.ClassSetting(mKey)
 end
 
+local Voidform = {
+	--Void Bolt on cooldown.
+	{'Void Eruption'},
+	--Shadow Word: Death when available on targets with <= 20% health.
+	{'Shadow Word: Death', 'target.health <= 20'},
+	--Mind Blast on cooldown.
+	{'Mind Blast'},
+	--Mind Flay as a filler.
+	{'Mind Flay'}
+}
 
-local ouCombat = {
+local ST = {
+	--Void Eruption at 100 Insanity to activate Voidform.
+	{'Void Eruption', 'player.insanity >= 100'},
+	--Shadow Word: Pain maintained at all times.
+	{'Shadow Word: Pain', 'target.debuff(Shadow Word: Pain).duration < 5'},
+	--Vampiric Touch maintained at all times.
+	{'Vampiric Touch', 'target.debuff(Vampiric Touch).duration < 5'},
+	--Shadow Word: Death when available on targets with <= 20% health.
+	{'Shadow Word: Death', 'target.health <= 20'},
+	--Mind Blast on cooldown to build Insanity.
+	{'Mind Blast'},
+	--Mind Flay as a filler to build Insanity.
+	{'Mind Flay'}
+}
+
+local outCombat = {
 
 }
 
 NeP.Engine.registerRotation(258, '[|cff'..MTS.Interface.addonColor..'MTS|r] Priest - Shadow', 
 	{-- In-Combat
-
-	},ouCombat, lib)
+		{Voidform, 'player.buff(Voidform)'},
+		{ST, '!player.buff(Voidform)'}
+	},outCombat, lib)
