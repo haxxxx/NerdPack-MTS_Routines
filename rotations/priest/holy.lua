2099143ad40fd2299104b95464925cb1d6241c5e
Jaime Marques <mrthesoulz@gmail.com>
2016-08-19 23:24:48 +0100

    Priest-Holy Tweaks

--- rotations/priest/holy.lua
+++ rotations/priest/holy.lua
@@ -21,9 +21,19 @@ local lib = function()
 	MTS.ClassSetting(mKey)
 end
 
+local keybinds = {
+	{'pause', 'modifier.shift'},
+	{'Angelic Fearther', 'modifier.control'}
+}
+
 local SpiritOfRedemption = {
 	{'Holy Word: Serenity', 'lowest.health < 50', 'lowest'},
-	{'Flash Heal', 'lowest.health < 100', 'lowest'},
+	{'Flash Heal', 'lowest.health < 100', 'lowest'}
+}
+
+local FastHeals = {
+	{'!Holy Word: Serenity', nil, 'lowest'},
+	{'!Flash Heal', nil, 'lowest'}
 }
 
 local Tank = {
@@ -37,19 +47,21 @@ local Player = {
 	{'Holy Word: Serenity', 'player.health < 30', 'player'},
 	{'Flash Heal', 'player.health < 60', 'player'},
 	{'Renew', '!player.buff(Renew)', 'player'},
+	{'Prayer of Mending', 'lowest.AoEHeal(90, 3, 20)'},
 	{'Prayer of Healing', 'player.AoEHeal(60, 4, 20)', 'lowest'},
 }
 
 local Lowest = {
 	{'Holy Word: Serenity', 'lowest.health < 25', 'lowest'},
 	{'Flash Heal', 'lowest.health < 50', 'lowest'},
-	{'Prayer of Healing', 'lowest.AoEHeal(60, 4, 20)', 'lowest'},
+	{'Prayer of Mending', 'lowest.AoEHeal(90, 3, 20)'},
+	{'Prayer of Healing', 'lowest.AoEHeal(60, 3, 20)', 'lowest'},
 	{'Renew', '!lowest.buff(Renew)', 'lowest'},
-	--{'Prayer of Mending', 'lowest.health < 100'},
-	{'Heal', 'lowest.health < 100', 'lowest'}
+	{'Heal', nil, 'lowest'}
 }
 
 local outCombat = {
+	{keybinds},
 	{'Renew', {'!lowest.buff(Renew)', 'lowest.health < 100'}, 'lowest'},
 	{'Heal', 'lowest.health < 100', 'lowest'},
 }
@@ -60,8 +72,11 @@ local moving = {
 
 NeP.Engine.registerRotation(257, '[|cff'..MTS.Interface.addonColor..'MTS|r] Priest - Holy', 
 	{-- In-Combat
+		{keybinds},
 		{SpiritOfRedemption, 'player.buff(Spirit of Redemption)'},
+		{'Purify', 'dispellAll(Purity)'},
 		{{ -- Not moving
+			{FastHeals, 'lowest.health < 30'},
 			{Tank, 'tank.health < 100'},
 			{Player, 'player.health < 100'},
 			{Lowest, 'lowest.health < 100'}
