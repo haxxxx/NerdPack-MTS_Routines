bbe8ac28b5a892d88385631f86d96245ae1080be
Jaime Marques <mrthesoulz@gmail.com>
2016-08-27 17:12:38 +0100

    warlock placeholders

---
+++ rotations/warlock/affliction.lua
@@ -0,0 +1,57 @@
+local mKey = 'MTS_WarlocAff'
+local config = {
+	key = mKey,
+	profiles = true,
+	title = '|T'..MTS.Interface.Logo..':10:10|t MTS Config',
+	subtitle = 'Warlock - Affliction Settings',
+	color = (function() return NeP.Core.classColor('player') end),
+	width = 250,
+	height = 500,
+	config = {
+		
+	}
+}
+
+local E = MTS.dynEval
+local F = function(key) return NeP.Interface.fetchKey(mKey, key, 100) end
+
+local exeOnLoad = function()
+	MTS.Splash()
+	NeP.Interface.buildGUI(config)
+	MTS.ClassSetting(mKey)
+end
+
+local Survival = {
+
+}
+
+local Cooldowns = {
+
+}
+
+local AoE = {
+
+}
+
+
+local ST = {
+
+}
+
+local Keybinds = {
+	-- Pause
+	{'pause', 'modifier.alt'},
+}
+
+local outCombat = {
+	{Keybinds},
+}
+
+NeP.Engine.registerRotation(265, '[|cff'..MTS.Interface.addonColor..'MTS|r] Warlock - Affliction', 
+	{-- In-Combat
+		{Keybinds},
+		{Survival, 'player.health < 100'},
+		{Cooldowns, 'modifier.cooldowns'},
+		{AoE, {'toggle.AoE', 'player.area(8).enemies >= 3'}},
+		{ST, {'target.range < 40', 'target.infront'}}
+	}, outCombat, exeOnLoad)
