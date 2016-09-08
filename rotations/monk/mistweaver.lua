006412a8fb31669a14c0704f86ee99e22566646c
Jaime Marques <mrthesoulz@gmail.com>
2016-09-08 09:56:41 +0100

    Monk cr "bones"

---
+++ rotations/monk/mistweaver.lua
@@ -0,0 +1,51 @@
+local mKey = 'MTSMonkWind'
+local config = {
+	key = mKey,
+	profiles = true,
+	title = '|T'..MTS.Interface.Logo..':10:10|t MTS Config',
+	subtitle = 'Monk - Mistweaver Settings',
+	color = (function() return NeP.Core.classColor('player') end),
+	width = 250,
+	height = 500,
+	config = {
+		
+	}
+}
+
+local E = MTS.dynEval
+local F = function(key) return NeP.Interface.fetchKey(mKey, key, 100) end
+
+local exeOnLoad = function()
+	MTS.Splash()
+	NeP.Interface.buildGUI(config)
+	MTS.ClassSetting(mKey)
+end
+
+local Tank = {
+
+}
+
+local Player = {
+
+}
+
+local Lowest = {
+
+}
+
+local Keybinds = {
+	-- Pause
+	{'%pause', 'modifier.alt'},
+}
+
+local outCombat = {
+	{Keybinds},
+}
+
+NeP.Engine.registerRotation(270, '[|cff'..MTS.Interface.addonColor..'MTS|r] Monk - Mistweaver', 
+	{-- In-Combat
+		{Keybinds},
+		{Tank, 'tank.health < 100'},
+		{Player, 'player.health < 100'},
+		{Lowest, 'lowest.health < 100'},
+	}, outCombat, exeOnLoad)
