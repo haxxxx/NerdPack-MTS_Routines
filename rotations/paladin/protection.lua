865348930a05698b7fadb7a982dcda51b9fd5581
Jaime Marques <mrthesoulz@gmail.com>
2016-09-14 01:23:19 +0100

    pala prot

--- rotations/paladin/protection.lua
+++ rotations/paladin/protection.lua
@@ -24,57 +24,95 @@ local exeOnLoad = function()
 	MTS.ClassSetting(mKey)
 end
 
+local Mitigation = {
+	--actions.prot+=/divine_steed,if=talent.knight_templar.enabled&incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
+	--actions.prot+=/eye_of_tyr,if=incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
+	--actions.prot+=/aegis_of_light,if=incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
+	--actions.prot+=/guardian_of_ancient_kings,if=incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
+	--actions.prot+=/divine_shield,if=talent.final_stand.enabled&incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
+	--actions.prot+=/ardent_defender,if=incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
+}
+
 local Survival = {
-	{'Light of the Protector', (function() return E('player.health <= '..F('P_LP')) end)},
-	{'Lay on Hands', (function() return E('player.health <= '..F('P_LH')) end)},
-	{'Divine Protection', (function() return E('player.health <= '..F('P_DP')) end)},
-	{'Ardent Defender', (function() return E('player.health <= '..F('P_AD')) end)}
+	--actions.prot=seraphim,if=talent.seraphim.enabled&action.shield_of_the_righteous.charges>=2
+	{'Seraphim', 'spell(Shield of the Righteous).charges >= 2'},
+	--actions.prot+=/shield_of_the_righteous,if=(!talent.seraphim.enabled|action.shield_of_the_righteous.charges>2)&!(debuff.eye_of_tyr.up&buff.aegis_of_light.up&buff.ardent_defender.up&buff.guardian_of_ancient_kings.up&buff.divine_shield.up&buff.potion.up)
+	{'Shield of the Righteous', {
+		{'!talent(7,2)', 'or', 'spell(Shield of the Righteous).charges > 2'},
+		{'!player.debuff(Eye of Tyr)', '!player.buff(Aegis of Light)', '!player.buff(Ardent Defender)', '!player.buff(Guardian of Ancient Kings)', '!player.buff(Divine Shield)'}
+	}, 'target' },
+	--actions.prot+=/bastion_of_light,if=talent.bastion_of_light.enabled&action.shield_of_the_righteous.charges<1
+	{'Bastion of Light', 'spell(Shield of the Righteous).charges < 1'},
+	{{
+		--actions.prot+=/light_of_the_protector,if=(health.pct<40)
+		{'Light of the Protector'},
+		--actions.prot+=/hand_of_the_protector,if=(health.pct<40)
+		{'Hand of the Protector'},
+	}, 'player.health < 40' },
+	
+	--actions.prot+=/light_of_the_protector,if=(incoming_damage_10000ms<health.max*1.25)&health.pct<55&talent.righteous_protector.enabled
+	--actions.prot+=/light_of_the_protector,if=(incoming_damage_13000ms<health.max*1.6)&health.pct<55
+	--actions.prot+=/hand_of_the_protector,if=(incoming_damage_6000ms<health.max*0.7)&health.pct<65&talent.righteous_protector.enabled
+	--actions.prot+=/hand_of_the_protector,if=(incoming_damage_9000ms<health.max*1.2)&health.pct<55
+
+	{Mitigation, {'!player.debuff(Eye of Tyr)', '!player.buff(Aegis of Light)', '!player.buff(Ardent Defender)', '!player.buff(Guardian of Ancient Kings)', '!player.buff(Divine Shield)'}},
+	
+	--actions.prot+=/lay_on_hands,if=health.pct<15
+	{'Lay on Hands', 'player.health < 15'},
+	
+	--actions.prot+=/potion,name=unbending_potion
+	--actions.prot+=/potion,name=draenic_strength,if=incoming_damage_2500ms>health.max*0.4&&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)|target.time_to_die<=25
+	--actions.prot+=/stoneform,if=incoming_damage_2500ms>health.max*0.4&!(debuff.eye_of_tyr.up|buff.aegis_of_light.up|buff.ardent_defender.up|buff.guardian_of_ancient_kings.up|buff.divine_shield.up|buff.potion.up)
 }
 
 local Interrupts = {
 	{'Rebuke'}
 }
 
-local Cooldowns = {
-}
-
 local AoE = {
-	--Consecration on cooldown.
-	{'Consecration', 'target.range <= 5'},
-	--Hammer of the Righteous while standing in Consecration.
-	{'Hammer of the Righteous', 'player.buff(Consecration)'}
+	--actions.prot_aoe=avengers_shield
+	--actions.prot_aoe+=/blessed_hammer
+	--actions.prot_aoe+=/judgment
+	--actions.prot_aoe+=/consecration
+	--actions.prot_aoe+=/hammer_of_the_righteous
 }
 
 local ST = {
-	--Shield of the Righteous to reduce damage taken.
-	{'Shield of the Righteous', {
-		'!player.buff', 
-		'player.health < 90',
-		'player.buff(Consecration)'
-	}},
-	{'Shield of the Righteous', {
-		'!player.buff', 
-		'player.health < 60',
-	}},
-	--Judgment on cooldown to further reduce the cooldown of Shield of the Righteous.
+	--actions.prot+=/avenging_wrath,if=!talent.seraphim.enabled
+	{'Avenging Wrath', '!talent(7,2)'},
+	--actions.prot+=/avenging_wrath,if=talent.seraphim.enabled&buff.seraphim.up
+	{'Avenging Wrath', {'talent(7,2)', 'player.buff(Seraphim)'}},
+	--actions.prot+=/judgment
 	{'Judgment'},
-	--Hammer of the Righteous when available to proc Grand Crusader.
-	{'Hammer of the Righteous', '!player.buff(Grand Crusader)'},
-	--Avenger's Shield with or without Grand Crusader.
+	--actions.prot+=/blessed_hammer
+	{'Blessed Hammer'},
+	--actions.prot+=/avengers_shield
 	{'Avenger\'s Shield'},
-	--Consecration on cooldown.
-	{'Consecration', 'target.range <= 5'}
+	--actions.prot+=/consecration
+	{'Consecration', 'target.range < 7'},
+	--actions.prot+=/blinding_light
+	{'Blinding Light'},
+	--actions.prot+=/hammer_of_the_righteous
+	{'Hammer of the Righteous'},
+}
+
+local inCombat = {
+	{Survival, "player.health < 100"},
+	{Interrupts, 'target.interruptsAt(50)'},
+	{Cooldowns, "toggle(cooldowns)"},
+	{AoE, {'toggle(AoE)', 'player.area(8).enemies >= 3'}},
+	{ST, 'target.infront'}
 }
 
 local outCombat = {
-
+	--# Executed before combat begins. Accepts non-harmful actions only.
+	--actions.precombat=flask,type=flask_of_ten_thousand_scars
+	--actions.precombat+=/flask,type=flask_of_the_countless_armies,if=role.attack|using_apl.max_dps
+	--actions.precombat+=/food,type=seedbattered_fish_plate
+	--actions.precombat+=/food,type=azshari_salad,if=role.attack|using_apl.max_dps
+	--# Snapshot raid buffed stats before combat begins and pre-potting is done.
+	--actions.precombat+=/snapshot_stats
+	--actions.precombat+=/potion,name=unbending_potion
 }
 
-NeP.Engine.registerRotation(66, '[|cff'..MTS.Interface.addonColor..'MTS|r] Paladin - Protection', 
-	{ -- In-Combat
-		{Survival, "player.health < 100"},
-		{Interrupts, 'target.interruptsAt(50)'},
-		{Cooldowns, "toggle(cooldowns)"},
-		{AoE, {'toggle(AoE)', 'player.area(8).enemies >= 3'}},
-		{ST, 'target.infront'}
-	}, outCombat, exeOnLoad)
+NeP.Engine.registerRotation(66, '[|cff'..MTS.Interface.addonColor..'MTS|r] Paladin - Protection', inCombat, outCombat, exeOnLoad)
