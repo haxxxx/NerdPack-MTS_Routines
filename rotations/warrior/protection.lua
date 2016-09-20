7a4bae331fb99ed6b544af2c7e0a1bb17647daf0
Jaime Marques <mrthesoulz@gmail.com>
2016-09-20 04:31:20 +0100

    Class UI stuff

--- rotations/warrior/protection.lua
+++ rotations/warrior/protection.lua
@@ -1,24 +1,9 @@
-local mKey = 'MTS_Warr_Prot'
-local config = {
-	key = mKey,
-	profiles = true,
-	title = '|T'..MTS.Interface.Logo..':10:10|t MTS Config',
-	subtitle = 'Warrior - Protection Settings',
-	color = (function() return NeP.Core.classColor('player') end),
-	width = 250,
-	height = 500,
-	config = {
+local GUI = {
 
-	}
 }
-
-local E = MTS.dynEval
-local F = function(key) return NeP.Interface.fetchKey(mKey, key, 100) end
 
 local exeOnLoad = function()
 	MTS.Splash()
-	NeP.Interface.buildGUI(config)
-	MTS.ClassSetting(mKey)
 end
 
 local Survival = {
@@ -80,6 +65,14 @@ local Keybinds = {
 	{'%pause', 'keybind(alt)'},
 }
 
+local inCombat = {
+	{Keybinds},
+	{Survival, 'player.health < 100'},
+	{Cooldowns, 'toggle(cooldowns)'},
+	{AoE, {'toggle(AoE)', 'player.area(8).enemies >= 3'}},
+	{ST, {'target.range < 8', 'target.infront'}}
+}
+
 local outCombat = {
 	{Keybinds},
 	--# Executed before combat begins. Accepts non-harmful actions only.
@@ -91,11 +84,4 @@ local outCombat = {
 	--actions.precombat+=/potion,name=unbending_potion
 }
 
-NeP.Engine.registerRotation(73, '[|cff'..MTS.Interface.addonColor..'MTS|r] Warrior - Protection', 
-	{-- In-Combat
-		{Keybinds},
-		{Survival, 'player.health < 100'},
-		{Cooldowns, 'toggle(cooldowns)'},
-		{AoE, {'toggle(AoE)', 'player.area(8).enemies >= 3'}},
-		{ST, {'target.range < 8', 'target.infront'}}
-	}, outCombat, exeOnLoad)
+NeP.Engine.registerRotation(73, '[|cff'..MTS.Interface.addonColor..'MTS|r] Warrior - Protection', inCombat, outCombat, exeOnLoad, GUI)
