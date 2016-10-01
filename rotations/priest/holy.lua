4ad2239ad0516d5816a7fc3bfbd759bf27539e51
Jaime Marques <mrthesoulz@gmail.com>
2016-10-01 14:17:24 +0100

    Priest holy tweaks

--- rotations/priest/holy.lua
+++ rotations/priest/holy.lua
@@ -8,7 +8,7 @@ end
 
 local keybinds = {
 	{'%pause', 'keybind(shift)'},
-	{'Angelic Feather', 'keybind(control)'}
+	{'Angelic Feather', 'keybind(control)', 'mouseover.ground'}
 }
 
 local SpiritOfRedemption = {
@@ -25,6 +25,7 @@ local Tank = {
 	{'Holy Word: Serenity', 'tank.health < 30', 'tank'},
 	{'Flash Heal', 'tank.health < 60', 'tank'},
 	{'Renew', '!tank.buff(Renew)', 'tank'},
+	{'Prayer of Mending', 'tank.AoEHeal(90, 3, 20)', 'tank'},
 	{'Prayer of Healing', 'tank.AoEHeal(60, 4, 20)', 'lowest'},
 }
 
@@ -32,40 +33,40 @@ local Player = {
 	{'Holy Word: Serenity', 'player.health < 30', 'player'},
 	{'Flash Heal', 'player.health < 60', 'player'},
 	{'Renew', '!player.buff(Renew)', 'player'},
-	{'Prayer of Mending', 'lowest.AoEHeal(90, 3, 20)'},
 	{'Prayer of Healing', 'player.AoEHeal(60, 4, 20)', 'lowest'},
 }
 
 local Lowest = {
 	{'Holy Word: Serenity', 'lowest.health < 25', 'lowest'},
 	{'Flash Heal', 'lowest.health < 50', 'lowest'},
-	{'Prayer of Mending', 'lowest.AoEHeal(90, 3, 20)'},
 	{'Prayer of Healing', 'lowest.AoEHeal(60, 3, 20)', 'lowest'},
 	{'Renew', '!lowest.buff(Renew)', 'lowest'},
 	{'Heal', nil, 'lowest'}
 }
 
+local Moving = {
+	{'Renew', {'!lowest.buff(Renew)', 'lowest.health < 100'}, 'lowest'}
+}
+
 local inCombat = {
 	{keybinds},
-	{SpiritOfRedemption, 'player.buff(Spirit of Redemption)'},
+	{SpiritOfRedemption, 'buff(Spirit of Redemption)'},
 	{'%dispelAll'},
 	{{ -- Not moving
 		{FastHeals, 'lowest.health < 30'},
 		{Tank, 'tank.health < 100'},
-		{Player, 'player.health < 100'},
+		{Player, 'health < 100'},
 		{Lowest, 'lowest.health < 100'}
-	}, '!player.moving' },
-	{moving, 'player.moving' }
+	}, '!moving' },
+	{Moving}
 }
 
 local outCombat = {
 	{keybinds},
-	{'Renew', {'!lowest.buff(Renew)', 'lowest.health < 100'}, 'lowest'},
-	{'Heal', 'lowest.health < 100', 'lowest'},
-}
-
-local moving = {
-	{'Renew', {'!lowest.buff(Renew)', 'lowest.health < 100'}, 'lowest'}
+	{{ -- Not moving
+		{'Renew', {'!lowest.buff(Renew)', 'lowest.health < 100'}, 'lowest'},
+		{'Heal', 'lowest.health < 100', 'lowest'},
+	}, '!moving' },
 }
 
 NeP.Engine.registerRotation(257, '[|cff'..MTSCR.Interface.addonColor..'MTS|r] Priest - Holy', inCombat, outCombat, exeOnLoad, GUI)
