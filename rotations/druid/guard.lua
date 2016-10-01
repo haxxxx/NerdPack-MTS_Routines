5dd5588db8931fa1ba89a5a0b98665b2c26e8032
Jaime Marques <mrthesoulz@gmail.com>
2016-10-01 15:50:35 +0100

    druid guard

--- rotations/druid/guard.lua
+++ rotations/druid/guard.lua
@@ -10,30 +10,42 @@ local Survival = {
 
 }
 
-local Cooldowns = {
-
-}
-
-local AoE = {
-	--Mangle whenever available to generate Rage. Watch for Gore procs.
-	{'Mangle'},
-	--Thrash on cooldown.
-	{'Thrash'},
-	--Moonfire on as many targets as possible.
-	{'Moonfire', 'target.debuff(Moonfire).duration < 5'},
-	--Swipe as often as possible.
-	{'Swipe'}
-}
 
 local ST = {
-	--Mangle whenever available to generate Rage. Watch for Gore procs.
-	{'Mangle'},
-	--Thrash on cooldown.
-	{'Thrash'},
-	--Moonfire to maintain the DoT.
-	{'Moonfire', 'target.debuff(Moonfire).duration < 5'},
-	--Maul to dump excess Rage
-	{'Maul'}
+	--actions=auto_attack
+	--actions+=/skull_bash
+	--actions+=/blood_fury
+	{'Blood Fury'},
+	--actions+=/berserking
+	{'Berserking'},
+	--actions+=/arcane_torrent
+	{'Arcane Torrent'},
+	--actions+=/use_item,slot=trinket2
+	{'#trinket2'},
+	--actions+=/barkskin
+	{'barkskin'},
+	--actions+=/bristling_fur,if=buff.ironfur.remains<2&rage<40
+	{'Bristling Fur', 'buff(Ironfur).duration<2&rage<40'},
+	--actions+=/ironfur,if=buff.ironfur.down|rage.deficit<25
+	{'Ironfur', '!buff'}
+	--actions+=/frenzied_regeneration,if=!ticking&incoming_damage_6s%health.max>0.25+(2-charges_fractional)*0.15
+	--actions+=/pulverize,cycle_targets=1,if=buff.pulverize.down
+	--actions+=/mangle
+	{'Mangle'}
+	--actions+=/pulverize,cycle_targets=1,if=buff.pulverize.remains<gcd
+	--actions+=/lunar_beam
+	{'Lunar Beam'}
+	--actions+=/incarnation
+	{'Incarnation'}
+	--actions+=/thrash_bear,if=active_enemies>=2
+	{'Trash', 'area(6).enemies>=2'},
+	--actions+=/pulverize,cycle_targets=1,if=buff.pulverize.remains<3.6
+	--actions+=/thrash_bear,if=talent.pulverize.enabled&buff.pulverize.remains<3.6
+	--actions+=/moonfire,cycle_targets=1,if=!ticking
+	--actions+=/moonfire,cycle_targets=1,if=remains<3.6
+	--actions+=/moonfire,cycle_targets=1,if=remains<7.2
+	--actions+=/moonfire
+	{'Moonfire'}
 }
 
 local Keybinds = {
@@ -44,8 +56,6 @@ local Keybinds = {
 local inCombat = {
 	{Keybinds},
 	{Survival, 'player.health < 100'},
-	{Cooldowns, 'toggle(cooldowns)'},
-	{AoE, {'toggle(AoE)', 'player.area(8).enemies >= 3'}},
 	{ST, {'target.range < 8', 'target.infront'}}
 }
 
